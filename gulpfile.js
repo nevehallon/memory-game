49d07b090bd663dfaa58f8ee23614a1e637f7cb9
Neveh Allon <NevehAllon@gmail.com>
2018-08-13 14:24:07 +0300

    Working App

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
  
 gulp.task('deploy', function() {
-  return gulp.src('./memory-game/**/*')
+  return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
